6c1944d5d1be093892bbc739244581d5fb2b650b
karthik153 <41208134+karthik153@users.noreply.github.com>
2026-09-06 11:50:11 +0530

    Fix source command syntax in Build ACE BAR stage of acev13Pipeline.groovy

--- vars/acev13Pipeline.groovy
+++ vars/acev13Pipeline.groovy
@@ -24,7 +24,7 @@ def call(Map pipelineParams) {
                 steps {
                     echo 'Building ACE BAR...'
                     sh """
-                        source ${PROFILE_PATH}
+                        . ${PROFILE_PATH}
                         mqsicreatebar -data . -b ${BAR_NAME} -a ${APP_NAME}
                     """
                 }
